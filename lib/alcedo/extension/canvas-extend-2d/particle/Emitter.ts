e163e3b20c4efb6dab600b2ea0d8280ff5138ab1
tommyZZM <452490446@qq.com>
2015-04-24 14:47:46 +0800

    [canvas]更新目录结构

---
+++ lib/alcedo/extension/canvas-extend-2d/particle/Emitter.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by tommyZZM on 2015/4/24.
+ * 离子发射器
+ */
+module alcedo{
+    export module canvas{
+        export class Emitter {
+
+        }
+    }
+}
---
+++ lib/alcedo/extension/canvas-extend-2d/particle/Particle.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by tommyZZM on 2015/4/24.
+ * 一枚单独的栗子
+ */
+module alcedo{
+    export module canvas{
+        export class Particle {
+
+        }
+    }
+}
