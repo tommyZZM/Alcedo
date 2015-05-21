e0105b5dfdca1defe77b414bd6498e231bbda5b0
tommyZZM <452490446@qq.com>
2015-05-22 00:03:16 +0800

    关卡模块

--- src-demo/game/core/LevelManager.ts
+++ src-demo/game/core/LevelManager.ts
@@ -5,19 +5,24 @@ module game {
     /**
      * 游戏关卡,地图管理器...
      */
+    var CHECK_DELAY:number = 20;
+    var MAX_LEVE_COUNT:number = 3;
+
     export class LevelManager extends alcedo.AppSubCore {
 
         private _root:alcedo.canvas.DisplatObjectContainer;
         private _levelobjs:Array<any>;
         private _levels:Dict;
 
-        private CHECK_DELAY:number = 20;
         private _checkdelay:number = 0;
+
+        private _levelpassed:number = 0;
 
         public startUp(root:canvas.DisplatObjectContainer){
             this._root = root;
             this._levelobjs = [];
             this._levels = new Dict();
+            this._activelevels = [];
             var levels = alcedo.core(net.AsyncRES).find(/level_\w+/i);
             if(levels.length>0){
                 for(var i=0;i<levels.length;i++){
@@ -30,24 +35,39 @@ module game {
             stage.addEventListener(alcedo.canvas.Stage.ENTER_SECOND,this.update,this);
         }
 
-        private currlevel:Level;
+        private _activelevels:Array<any>;
         private update(e){
 
-            if(this._checkdelay===this.CHECK_DELAY && this._runstate){
+            var levelobj:any;
+            var level:Level;
+            if(this._checkdelay===CHECK_DELAY && this._runstate){
 
+                if(this._activelevels.length<MAX_LEVE_COUNT){
+                    levelobj = this._levelobjs.randomselect();
+                    if(this._activelevels.length===0){
+
+                        level = new Level(levelobj)
+                        level.x = 660;
+                        this._root.addChild(level)
+                        trace("start..");
+
+                        //首次创建
+                    }else{
+
+                    }
+                }
             }
 
             this._checkdelay++;
-            if(this._checkdelay>this.CHECK_DELAY){
+            if(this._checkdelay>CHECK_DELAY){
                 this._checkdelay = 0;
             }
         }
 
         private _runstate:boolean;
         public run(){
+            this._checkdelay = CHECK_DELAY
             this._runstate = true;
-
-            trace(this._levelobjs.randomselect());
         }
 
         public stop(){
@@ -55,8 +75,8 @@ module game {
         }
 
         public reset(){
-            this._levels.clear();
-
+            this._activelevels = [];
+            this._levelpassed = 0;
         }
 
         public get root():canvas.DisplatObjectContainer{
--- src-demo/game/core/class/Level.ts
+++ src-demo/game/core/class/Level.ts
@@ -12,6 +12,22 @@ module game {
             super();
             if(!levelconfig)warn("level config invaild");
             this._levelconfig = levelconfig;
+            this.width = this._levelconfig.width;
+            this.height = this._levelconfig.height;
+
+            this.debugArea(true);
+        }
+
+
+        private _debugdraw:alcedo.canvas.graphic.Rectangle;
+        private debugArea(active:boolean){
+            if(!this._debugdraw) {
+                this._debugdraw = new alcedo.canvas.graphic.Rectangle(0, 0, this._levelconfig.pixelwidth, this._levelconfig.pixelheight, "#27AE60");
+                this.addChild(this._debugdraw);
+                this._debugdraw.alpha = 0.2
+            }
+            this._debugdraw.visible = active;
+            this.setChildIndex(this._debugdraw,0)
         }
     }
 }
